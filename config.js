8b44aa803eac5be5752714b8777af0c25b2102b7
Kumabear7112003 <91521276+Kumabear7112003@users.noreply.github.com>
2021-09-28 13:28:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ const CONFIG = {
     introDesc: `Nắng kia của một bầu trời
     Còn anh đã của ai rồi hay chưa?
     Mây kia là của hạt mưa
-    Anh xem đã thích em chưa hay rồi`,
+    Anh xem đã thích em chưa hay rồi?`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn luôn thích em <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnNo: 'Không, mơ ra à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại thích tớ <3',
+    btnReply: 'Gửi cho tớ <3',
     reply: 'Thế giới tớ đã luôn chỉ có bạn từ lâu',
     mess: 'Tớ biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Cố gắng học lên nhé.',
